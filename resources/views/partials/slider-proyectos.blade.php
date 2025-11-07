4d0ef89ea090c6e6e3400125a0b8eb8acc7374d3
Mersmith <mersmith14@gmail.com>
2025-11-07 18:02:12 -0500

    avance slider proyectos

---
+++ resources/views/partials/slider-proyectos.blade.php
@@ -0,0 +1,55 @@
+@if (!empty($p_elemento) && $p_elemento['posts']->isNotEmpty())
+
+<div class="partials_contenedor_slider_proyectos">
+    <!-- Swiper -->
+    <div class="swiper SwiperSliderProyectos-{{ $p_elemento['id'] }}">
+        <div class="swiper-wrapper">
+            @foreach ($p_elemento['posts'] as $post)
+            <div class="swiper-slide">
+                <a href="{{ route('blog.show', $post->slug) }}">
+                    <div class="post_imagen_contenedor">
+                        <img src="{{ $post->imagen }}">
+                        <p class="titulo">{{ $post->meta_title }}</p>
+                        <div class="pie">
+                            <p class="descripcion"> <i class="fa-solid fa-arrow-up"></i> {{ $post->meta_description }}
+                            </p>
+
+                            <p class="descripcion"> <i class="fa-solid fa-arrow-up"></i> {{ $post->meta_description }}
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+
+<script>
+    var swiper = new Swiper('.SwiperSliderProyectos-{{ $p_elemento['id'] }}', {
+            slidesPerView: 3.5, // 👈 muestra 3 completos + un poco del siguiente
+            spaceBetween: 20, // espacio entre slides
+            autoplay: {
+                delay: 3500,
+                disableOnInteraction: false,
+            },
+            loop: true,
+            grabCursor: true,
+
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3.5,
+                    spaceBetween: 20,
+                },
+                768: {
+                    slidesPerView: 2.5,
+                    spaceBetween: 20,
+                },
+                0: {
+                    slidesPerView: 1.2,
+                    spaceBetween: 20,
+                }
+            }
+        });
+</script>
+@endif
--- resources/views/web/inicio.blade.php
+++ resources/views/web/inicio.blade.php
@@ -8,6 +8,13 @@
 
 <div class="r_centrar_pagina">
 
+    @include('partials.titulo-encabezado', [
+    'titulo' => 'Nuestros proyectos',
+    'alineacion' => 'left',
+    ])
+
+    @include('partials.slider-proyectos', ['p_elemento' => $posts])
+
     @include('bloques.bloque-8', ['p_elemento' => $bloque8_1])
 
     @include('bloques.bloque-4', ['p_elemento' => $bloque4_1])
