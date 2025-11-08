628952a6e6f94b471a89091c6597e4d70603ed11
Mersmith <mersmith14@gmail.com>
2025-11-08 12:05:55 -0500

    avance titulo

--- resources/views/partials/titulo-encabezado.blade.php
+++ resources/views/partials/titulo-encabezado.blade.php
@@ -1,8 +1,8 @@
-<div class="partials_titulo_encabezado r_titulo_1">
+<div class="partials_titulo_encabezado ">
     @if (!empty($titulo))
-    <h2 class="{{ $color }}" style="text-align: {{ $alineacion }}">{!! $titulo !!}</h2> @endif
+    <h2 class="r_titulo_1 {{ $color }}" style="text-align: {{ $alineacion }}">{!! $titulo !!}</h2> @endif
 
     @if (!empty($descripcion))
-    <p class="{{ $color }}" style="text-align: {{ $alineacion }}">{!! $descripcion !!}</p>
+    <p class="r_parrafo {{ $color }}" style="text-align: {{ $alineacion }}">{!! $descripcion !!}</p>
     @endif
 </div>
