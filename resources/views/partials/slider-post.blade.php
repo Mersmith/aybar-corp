55a2eba0e657dc3d23be0ea94df101e667005232
Mersmith <mersmith14@gmail.com>
2025-11-09 08:40:43 -0500

    avance noticias

--- resources/views/partials/slider-post.blade.php
+++ resources/views/partials/slider-post.blade.php
@@ -1,34 +1,42 @@
 @if (!empty($p_elemento) && $p_elemento['posts']->isNotEmpty())
 
-<div class="partials_contenedor_slider_post">
-    <!-- Swiper -->
-    <div class="swiper SwiperSliderPost-{{ $p_elemento['id'] }}">
-        <div class="swiper-wrapper">
-            @foreach ($p_elemento['posts'] as $post)
-            <div class="swiper-slide">
-                <a href="{{ route('blog.show', $post->slug) }}">
-                    <div class="post_imagen_contenedor">
-                        <img src="{{ $post->imagen }}">
-                        <p class="titulo">{{ $post->meta_title }}</p>
-                        <p class="fecha">{{ $post->created_at->format('d M Y') }}</p>
-                        <p class="descripcion">{{ $post->meta_description }}</p>
+    <div class="partials_contenedor_slider_post">
+        <!-- Swiper -->
+        <div class="swiper SwiperSliderPost-{{ $p_elemento['id'] }}">
+            <div class="swiper-wrapper">
+                @foreach ($p_elemento['posts'] as $post)
+                    <div class="swiper-slide">
+                        <a href="{{ route('blog.show', $post->slug) }}">
+                            <div class="post_imagen_contenedor">
+                                <img src="{{ $post->imagen }}">
+                                <div class="post_datos">
+                                    <div class="fecha">
+                                        <b>{{ $post->created_at->format('d') }}</b>
+                                        <p>{{ $post->created_at->format('M') }}</p>
+                                        <p>{{ $post->created_at->format('Y') }}</p>
+                                    </div>
+                                    <div class="datos">
+                                        <p class="titulo">{{ $post->meta_title }}</p>
+                                        <p class="descripcion">{{ $post->meta_description }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                </a>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
-</div>
 
-<script>
-    var swiper = new Swiper('.SwiperSliderPost-{{ $p_elemento['id'] }}', {
+    <script>
+        var swiper = new Swiper('.SwiperSliderPost-{{ $p_elemento['id'] }}', {
             slidesPerView: 3.5, // 👈 muestra 3 completos + un poco del siguiente
             spaceBetween: 20, // espacio entre slides
-            autoplay: {
+            /*autoplay: {
                 delay: 3500,
                 disableOnInteraction: false,
             },
-            loop: true,
+            loop: true,*/
             grabCursor: true,
 
             breakpoints: {
@@ -46,5 +54,5 @@
                 }
             }
         });
-</script>
+    </script>
 @endif
