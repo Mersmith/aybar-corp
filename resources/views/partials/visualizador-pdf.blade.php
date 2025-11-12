a16d04e09498a9e8b51be9f31033920e1ce0236f
Mersmith <mersmith14@gmail.com>
2025-11-12 16:36:50 -0500

    avance pdf

---
+++ resources/views/partials/visualizador-pdf.blade.php
@@ -0,0 +1,21 @@
+@props([
+'link' => '#',
+'titulo' => 'Libro Digital',
+'textoBoton' => 'Ver Libro Digital',
+])
+
+<div x-data="{ abierto: false }" x-effect="document.documentElement.classList.toggle('no-scroll', abierto)"
+    @keydown.escape.window="if (abierto) abierto = false" class="partials_visualizador_pdf">
+
+    <button @click="abierto = true" class="boton_abrir">
+        {{ $textoBoton }}
+    </button>
+
+    <div class="visor_pdf" x-show="abierto" x-transition.opacity.duration.200ms x-cloak :inert="!abierto">
+        <iframe id="pdfFrame" src="{{ $link }}" allowfullscreen loading="lazy"></iframe>
+
+        <button @click="abierto = false" class="barra_cerrar" aria-label="Cerrar libro">
+            <i class="fa-solid fa-xmark"></i>
+        </button>
+    </div>
+</div>
