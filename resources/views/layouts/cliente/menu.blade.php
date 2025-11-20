6e5c0ac94a6d88113c8aaf7e12ee8145cf43082b
Mersmith <mersmith14@gmail.com>
2025-11-20 17:28:49 -0500

    avance cliente

--- resources/views/layouts/cliente/menu.blade.php
+++ resources/views/layouts/cliente/menu.blade.php
@@ -1,37 +1,22 @@
 <div class="cliente_menu_pricipal">
-    <a href="{{ route('cliente.home') }}"
-        class="">
+    <a href="{{ route('cliente.home') }}" class="">
         <span><i class="fa-solid fa-address-card"></i>Perfil</span>
         <i class="fa-solid fa-chevron-right"></i>
     </a>
 
-    <a href=""
-        class="">
-        <span><i class="fa-solid fa-basket-shopping"></i> Mis Lotes</span>
-        <i class="fa-solid fa-chevron-right"></i>
-    </a>
-
-    <a href="{{ route('cliente.direccion') }}"
-        class="">
+    <a href="{{ route('cliente.direccion') }}" class="">
         <span><i class="fa-solid fa-map-location"></i> Direcciones</span>
         <i class="fa-solid fa-chevron-right"></i>
     </a>
 
-    <a href=""
-        class="">
-        <span><i class="fa-solid fa-arrow-rotate-left"></i> Cronograma</span>
+    <a href="" class="">
+        <span><i class="fa-solid fa-border-all"></i> Mis Lotes</span>
         <i class="fa-solid fa-chevron-right"></i>
     </a>
 
-    <a href=""
-        class="">
-        <span><i class="fa-solid fa-heart"></i> Comprobantes</span>
-        <i class="fa-solid fa-chevron-right"></i>
-    </a>
-
-    <form method="POST" action="" >
+    <form method="POST" action="{{ route('logout.cliente') }}">
         @csrf
-        <a href="" >
+        <a href="">
             <span><i class="fa-solid fa-power-off"></i> Cerrar</span>
         </a>
     </form>
