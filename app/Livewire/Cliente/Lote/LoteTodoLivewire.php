c7ea9cd40a44002e5b0483adf6fdd347882ade42
Mersmith <mersmith14@gmail.com>
2025-11-21 13:56:38 -0500

    lotes

--- app/Livewire/Cliente/Lote/LoteTodoLivewire.php
+++ app/Livewire/Cliente/Lote/LoteTodoLivewire.php
@@ -7,72 +7,102 @@ use Livewire\Component;
 class LoteTodoLivewire extends Component
 {
 
-    public $lotes = [
-        [
-            'razon_social'        => 'Razon 1',
-            'nombre_proyecto'    => 'Proyecto 1',
-            'manzana'            => 'Manzana 1',
-            'lote'               => 'Lote 1',
-            'cronograma_id'      => 'Cronograma id 1',
-            'estado_cuenta_id'   => 'Estado cuenta id 1',
-        ],
-        [
-            'razon_social'        => 'Razon 1',
-            'nombre_proyecto'    => 'Proyecto 2',
-            'manzana'            => 'Manzana 2',
-            'lote'               => 'Lote 2',
-            'cronograma_id'      => 'Cronograma id 2',
-            'estado_cuenta_id'   => 'Estado cuenta id 2',
-        ],
-        [
-            'razon_social'        => 'Razon 3',
-            'nombre_proyecto'    => 'Proyecto 3',
-            'manzana'            => 'Manzana 3',
-            'lote'               => 'Lote 3',
-            'cronograma_id'      => 'Cronograma id 3',
-            'estado_cuenta_id'   => 'Estado cuenta id 3',
-        ],
-        [
-            'razon_social'        => 'Razon 3',
-            'nombre_proyecto'    => 'Proyecto 4',
-            'manzana'            => 'Manzana 4',
-            'lote'               => 'Lote 4',
-            'cronograma_id'      => 'Cronograma id 4',
-            'estado_cuenta_id'   => 'Estado cuenta id 4',
-        ],
-        [
-            'razon_social'        => 'Razon 3',
-            'nombre_proyecto'    => 'Proyecto 5',
-            'manzana'            => 'Manzana 5',
-            'lote'               => 'Lote 5',
-            'cronograma_id'      => 'Cronograma id 5',
-            'estado_cuenta_id'   => 'Estado cuenta id 5',
-        ],
-        [
-            'razon_social'        => 'Razon 6',
-            'nombre_proyecto'    => 'Proyecto 6',
-            'manzana'            => 'Manzana 6',
-            'lote'               => 'Lote 6',
-            'cronograma_id'      => 'Cronograma id 6',
-            'estado_cuenta_id'   => 'Estado cuenta id 6',
-        ],
-        [
-            'razon_social'        => 'Razon 7',
-            'nombre_proyecto'    => 'Proyecto 7',
-            'manzana'            => 'Manzana 7',
-            'lote'               => 'Lote 7',
-            'cronograma_id'      => 'Cronograma id 7',
-            'estado_cuenta_id'   => 'Estado cuenta id 7',
-        ],
-        [
-            'razon_social'        => 'Razon 8',
-            'nombre_proyecto'    => 'Proyecto 8',
-            'manzana'            => 'Manzana 8',
-            'lote'               => 'Lote 8',
-            'cronograma_id'      => 'Cronograma id 8',
-            'estado_cuenta_id'   => 'Estado cuenta id 8',
-        ],
-    ];
+    public $razones_sociales = [];
+    public $razon_social = "";
+    public $lotes = [];
+    public $lotes_originales = [];
+
+
+    public function mount()
+    {
+        $this->lotes_originales =  [
+            [
+                'razon_social'        => 'Razon 1',
+                'nombre_proyecto'    => 'Proyecto 1',
+                'manzana'            => 'Manzana 1',
+                'lote'               => 'Lote 1',
+                'cronograma_id'      => 'Cronograma id 1',
+                'estado_cuenta_id'   => 'Estado cuenta id 1',
+            ],
+            [
+                'razon_social'        => 'Razon 1',
+                'nombre_proyecto'    => 'Proyecto 2',
+                'manzana'            => 'Manzana 2',
+                'lote'               => 'Lote 2',
+                'cronograma_id'      => 'Cronograma id 2',
+                'estado_cuenta_id'   => 'Estado cuenta id 2',
+            ],
+            [
+                'razon_social'        => 'Razon 3',
+                'nombre_proyecto'    => 'Proyecto 3',
+                'manzana'            => 'Manzana 3',
+                'lote'               => 'Lote 3',
+                'cronograma_id'      => 'Cronograma id 3',
+                'estado_cuenta_id'   => 'Estado cuenta id 3',
+            ],
+            [
+                'razon_social'        => 'Razon 3',
+                'nombre_proyecto'    => 'Proyecto 4',
+                'manzana'            => 'Manzana 4',
+                'lote'               => 'Lote 4',
+                'cronograma_id'      => 'Cronograma id 4',
+                'estado_cuenta_id'   => 'Estado cuenta id 4',
+            ],
+            [
+                'razon_social'        => 'Razon 3',
+                'nombre_proyecto'    => 'Proyecto 5',
+                'manzana'            => 'Manzana 5',
+                'lote'               => 'Lote 5',
+                'cronograma_id'      => 'Cronograma id 5',
+                'estado_cuenta_id'   => 'Estado cuenta id 5',
+            ],
+            [
+                'razon_social'        => 'Razon 6',
+                'nombre_proyecto'    => 'Proyecto 6',
+                'manzana'            => 'Manzana 6',
+                'lote'               => 'Lote 6',
+                'cronograma_id'      => 'Cronograma id 6',
+                'estado_cuenta_id'   => 'Estado cuenta id 6',
+            ],
+            [
+                'razon_social'        => 'Razon 7',
+                'nombre_proyecto'    => 'Proyecto 7',
+                'manzana'            => 'Manzana 7',
+                'lote'               => 'Lote 7',
+                'cronograma_id'      => 'Cronograma id 7',
+                'estado_cuenta_id'   => 'Estado cuenta id 7',
+            ],
+            [
+                'razon_social'        => 'Razon 8',
+                'nombre_proyecto'    => 'Proyecto 8',
+                'manzana'            => 'Manzana 8',
+                'lote'               => 'Lote 8',
+                'cronograma_id'      => 'Cronograma id 8',
+                'estado_cuenta_id'   => 'Estado cuenta id 8',
+            ],
+        ];
+
+        $this->lotes = $this->lotes_originales;
+
+        $this->razones_sociales = collect($this->lotes_originales)
+            ->pluck('razon_social')
+            ->unique()
+            ->values()
+            ->toArray();
+    }
+
+    public function updatedRazonSocial($value)
+    {
+        if ($value === "" || $value === "todos") {
+            $this->lotes = $this->lotes_originales;
+            return;
+        }
+
+        $this->lotes = collect($this->lotes_originales)
+            ->where('razon_social', $value)
+            ->values()
+            ->toArray();
+    }
 
     public function render()
     {
--- resources/views/livewire/cliente/lote/lote-todo-livewire.blade.php
+++ resources/views/livewire/cliente/lote/lote-todo-livewire.blade.php
@@ -1,68 +1,52 @@
-<div>
+<div class="r_gap_pagina">
     <div class="r_panel">
         <div class="r_titulo_panel">
             <h2>Mis lotes</h2>
         </div>
+
+        <div class="g_formulario">
+            <div class="r_fila">
+                <div class="r_columna_4">
+                    <select wire:model.live="razon_social" id="razon_social" name="razon_social">
+                        <option value="">Todos</option>
+                        @foreach ($razones_sociales as $item)
+                        <option value="{{ $item }}">{{ $item }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div class="tablecontainer">
-        <table>
-            <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Points</th>
-                <th>Points</th>
-                <th>Points</th>
-                <th>Points</th>
-                <th>Points</th>
-                <th>Points</th>
-                <th>Points</th>
-                <th>Points</th>
-                <th>Points</th>
-                <th>Points</th>
-            </tr>
-            <tr>
-                <td>Jill</td>
-                <td>Smith</td>
-                <td>50</td>
-                <td>50</td>
-                <td>50</td>
-                <td>50</td>
-                <td>50</td>
-                <td>50</td>
-                <td>50</td>
-                <td>50</td>
-                <td>50</td>
-                <td>50</td>
-            </tr>
-            <tr>
-                <td>Eve</td>
-                <td>Jackson</td>
-                <td>94</td>
-                <td>94</td>
-                <td>94</td>
-                <td>94</td>
-                <td>94</td>
-                <td>94</td>
-                <td>94</td>
-                <td>94</td>
-                <td>94</td>
-                <td>94</td>
-            </tr>
-            <tr>
-                <td>Adam</td>
-                <td>Johnson</td>
-                <td>67</td>
-                <td>67</td>
-                <td>67</td>
-                <td>67</td>
-                <td>67</td>
-                <td>67</td>
-                <td>67</td>
-                <td>67</td>
-                <td>67</td>
-                <td>67</td>
-            </tr>
-        </table>
+    <div class="tabla_contenido">
+        <div class="contenedor_tabla">
+            <table class="tabla">
+                <thead>
+                    <tr>
+                        <th>Razón social</th>
+                        <th>Proyecto</th>
+                        <th>Manzana</th>
+                        <th>Lote</th>
+                        <th>ID Cronograma</th>
+                        <th>ID Estado Cuenta</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($lotes as $lote)
+                    <tr>
+                        <td>{{ $lote['razon_social'] }}</td>
+                        <td>{{ $lote['nombre_proyecto'] }}</td>
+                        <td>{{ $lote['manzana'] }}</td>
+                        <td>{{ $lote['lote'] }}</td>
+                        <td><a href="{{ $lote['cronograma_id'] }}" class="boton boton_activo">Cronograma</a></td>
+                        <td><a href="{{ $lote['estado_cuenta_id'] }}" class="boton boton_guardar">Estado cuenta</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
+
+
 </div>
