f8eaa9678d9e13ea5bc7492cb9967c5784c6099d
Mersmith <mersmith14@gmail.com>
2025-12-06 11:52:56 -0500

    permiso crud

--- routes/spatie.php
+++ routes/spatie.php
@@ -8,6 +8,10 @@ use App\Livewire\Spatie\Rol\RolTodoLivewire;
 use App\Livewire\Spatie\Rol\RolCrearLivewire;
 use App\Livewire\Spatie\Rol\RolEditarLivewire;
 
+use App\Livewire\Spatie\Permiso\PermisoTodoLivewire;
+use App\Livewire\Spatie\Permiso\PermisoCrearLivewire;
+use App\Livewire\Spatie\Permiso\PermisoEditarLivewire;
+
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('usuario')->name('usuario.vista.')->group(function () {
@@ -20,4 +24,10 @@ Route::prefix('rol')->name('rol.vista.')->group(function () {
     Route::get('/', RolTodoLivewire::class)->name('todo');
     Route::get('/crear', RolCrearLivewire::class)->name('crear');
     Route::get('/editar/{id}', RolEditarLivewire::class)->name('editar');
+});
+
+Route::prefix('permiso')->name('permiso.vista.')->group(function () {
+    Route::get('/', PermisoTodoLivewire::class)->name('todo');
+    Route::get('/crear', PermisoCrearLivewire::class)->name('crear');
+    Route::get('/editar/{id}', PermisoEditarLivewire::class)->name('editar');
 });
