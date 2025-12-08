7449b111750de661da6acee13acbb42c8a31912a
Mersmith <mersmith14@gmail.com>
2025-12-07 21:05:52 -0500

    avance

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            //RolesYPermisosSeeder::class,
-            //UserSeeder::class,
+            RolesYPermisosSeeder::class,
+            UserSeeder::class,
 
             //EstadoComprobantePagoSeeder::class,
             //ComprobantePagoSeeder::class,
