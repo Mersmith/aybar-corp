d9a3e56668a9f1058ec73c45a0fa80a748a4f203
Mersmith <mersmith14@gmail.com>
2025-12-11 12:29:03 -0500

    formulario centrar

--- resources/views/partials/formulario-imagen.blade.php
+++ resources/views/partials/formulario-imagen.blade.php
@@ -1,12 +1,16 @@
 <div class="partials_formulario_imagen">
-    <img src="{{ asset('assets/imagenes/slider/sliders-computadora-3.jpg') }}" alt="">
+    <img class="imagen" src="{{ asset('assets/imagenes/slider/sliders-computadora-3.jpg') }}" alt="">
 
-    <div class="g_centrar_pagina">
+    <div class="cuerpo">
 
-        <div class="contenedor_contacto">
-            <div class="contenedor_formulario">
-                @livewire('web.contacto.formulario-contacto-livewire')
+        <div class="g_centrar_pagina">
+
+            <div class="contenedor_contacto">
+                <div class="contenedor_formulario">
+                    @livewire('web.contacto.formulario-contacto-livewire')
+                </div>
             </div>
         </div>
     </div>
+
 </div>
